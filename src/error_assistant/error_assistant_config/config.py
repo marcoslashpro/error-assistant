0a5a287897bde287ce40716434e64856c022ac96
marcoslashpro <tambascomarco35@gmail.com>
2025-04-16 17:35:11 +0200

    Fixed minor config path issue

--- src/error_assistant/error_assistant_config/config.py
+++ src/error_assistant/error_assistant_config/config.py
@@ -1,12 +1,14 @@
 import tomllib
 import os
 from pathlib import Path
+import error_assistant.error_assistant_config
 
 
 class Config:
     def __init__(self) -> None:
         # Dynamically resolve path relative to this file
-        self.CONFIG_PATH = Path(__file__).parent / "config.toml"
+        package_path = os.path.dirname(error_assistant.error_assistant_config.__file__)
+        self.CONFIG_PATH = Path(package_path) / "config.toml"
 
         # Load the configuration
         with open(self.CONFIG_PATH, 'rb') as f:
